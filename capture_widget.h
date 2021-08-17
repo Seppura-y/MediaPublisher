b91e3465ff1d62ed1e8b3372890039d0f87920e8
Hiko Y <y7513414@163.com>
2021-08-17 08:50:04 +0800

    123

--- capture_widget.h
+++ capture_widget.h
@@ -7,7 +7,6 @@
 #include "av_encode_handler.h"
 #include "av_mux_handler.h"
 #include "sdl_view.h"
-#include "rtmp_pusher.h"
 
 struct CaptureWidgetParameters
 {
@@ -15,7 +14,6 @@ struct CaptureWidgetParameters
 	int output_height_;
 	QString url_;
 };
-struct AVPacket;
 
 class CaptureWidget : public QWidget
 {
@@ -26,29 +24,16 @@ public:
 	~CaptureWidget();
 
 public slots:
-	void OnStartPush();
-	void OnResetParam(CaptureWidgetParameters);
-	void OnStopPush();
-
-	void OnSetIsLibrtmpMethod(bool);
-
-
+	void OnSignalPush(CaptureWidgetParameters);
+	void OnParametersSet(CaptureWidgetParameters);
+	void OnSignalStop();
 protected:
 	void paintEvent(QPaintEvent* ev) override;
 	void timerEvent(QTimerEvent* ev)override;
 	void resizeEvent(QResizeEvent* ev)override;
 
 	void DrawFrame();
-
-	void VideoEncodeCallback(AVPacket* v_pkt);
-	void AudioEncodeCallback(AVPacket* a_pkt);
-
-	bool IsVideoSeqHeaderNeeded();
-	bool IsAudioSeqHeaderNeeded();
-	void SetVideoSeqHeaderNeeded(bool status);
-	void SetAudioSeqHeaderNeeded(bool status);
 private:
-	bool is_librtmp_method_ = false;
 	int output_width_ = -1;
 	int output_height_ = -1;
 	QString url_;
@@ -58,7 +43,4 @@ private:
 	AVEncodeHandler* encode_handler_ = nullptr;
 	AVScreenCapHandler* capture_handler_ = nullptr;
 
-	bool is_video_seq_header_needed_ = true;
-	bool is_audio_seq_header_needed_ = true;
-	RtmpPusher* rtmp_pusher_ = nullptr;
 };
